deb48ab377c4979e632dcf1d57eab5a29fd79149
Wei Ouyang <oeway007@gmail.com>
2020-07-01 12:18:36 +0200

    fix body width

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 <style>
 html,
 body {
+  width: 100vw;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
   background: #dcdcdc;
