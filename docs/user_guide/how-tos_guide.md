909aebc5de74ec4133155ea94419df56b936b3d0
oeway <oeway@users.noreply.github.com>
2021-03-29 18:52:13 +0000

    Deploy to GitHub Pages a3df3a59e6f5b483fae3e788c46cd2faeadd08aa

---
+++ docs/user_guide/how-tos_guide.md
@@ -0,0 +1,37 @@
+<!-- docs/user_guide/how-tos_guide.md -->
+
+# How-To use bioimage.io 
+
+## DeepImageJ
+
+TBD
+
+## Fiji
+**Note: Fiji only supports Tensorflow 1 models at the moment!**
+
+1. Install the [CSBDeep-Plugin](https://github.com/CSBDeep/CSBDeep_website/wiki/CSBDeep-in-Fiji-%E2%80%93-Installation) in Fiji.
+This will give you all the necessary Plugins to run bioimage.io models at the moment.
+2. Open the image you want to run the model on
+3. a) If your model is a [CSBDeep](https://imagej.net/CSBDeep) one, go to Plugins > CSBDeep and choose either "N2V", "DenoiSeg" or "Run your network"(default)
+   
+   b) For any other bioimage.io model, go to Plugins > bioimage.io > bioimage.io prediction. 
+4.  Continuing from 3b) you will arrive at this window:
+
+<img src="user_guide/fiji_bioimage_predict.jpg" alt="Fiji bioimage.io prediction" width="60%"/>
+
+The configuration fields should be self-explanatory.
+
+5. Click "OK" to run the model prediction.
+
+
+## ilastik
+
+TBD
+
+##  ImJoy
+
+TBD
+
+## ZeroCostDL4Mic
+
+TBD
---
+++ precache-manifest.a6cded3564aaaf0886ecd40a0a300406.js
@@ -0,0 +1,394 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98fe0695fb0549bc5525a718425f0193",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "9db7f580073fc5dd0f07e984bc0bc3df",
+    "url": "/SMLM-Viewer.imjoy.html"
+  },
+  {
+    "revision": "380ef94820517c58ddc7",
+    "url": "/css/app.1d424d30.css"
+  },
+  {
+    "revision": "437993641c52d1ded2ea",
+    "url": "/css/chunk-vendors.4bdf99a1.css"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/fonts/materialdesignicons-webfont.6a2ddad1.ttf"
+  },
+  {
+    "revision": "8a67040660444e8d8ee82a406d580d8f",
+    "url": "/fonts/materialdesignicons-webfont.8a670406.eot"
+  },
+  {
+    "revision": "c32505e8c654310ece1da4e1c10eeb57",
+    "url": "/fonts/materialdesignicons-webfont.c32505e8.woff"
+  },
+  {
+    "revision": "dc85ceeb0daba687e36d8dde4ed4d352",
+    "url": "/fonts/materialdesignicons-webfont.dc85ceeb.woff2"
+  },
+  {
+    "revision": "68cbd983f60233ce71ce2c81b2a1208e",
+    "url": "/index.ejs"
+  },
+  {
+    "revision": "6054c5e0a7546b0a3a90b26e8a0d5eb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "380ef94820517c58ddc7",
+    "url": "/js/app.73b39056.js"
+  },
+  {
+    "revision": "437993641c52d1ded2ea",
+    "url": "/js/chunk-vendors.275ce9dd.js"
+  },
+  {
+    "revision": "65963beb11162b7a054156ac0318d2c8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "284807dd235bf17c9b9b4b9a7d8465e8",
+    "url": "/manifest.shareloc.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bbcfcbe5b9963396c6408dcff02f6c39",
+    "url": "/static/anonymousAnimals/Alligator.png"
+  },
+  {
+    "revision": "0de57e8aae0a4c9fd10bcc981d563577",
+    "url": "/static/anonymousAnimals/Anteater.png"
+  },
+  {
+    "revision": "83e6c666989c42b9a7c4be0643e4428d",
+    "url": "/static/anonymousAnimals/Armadillo.png"
+  },
+  {
+    "revision": "e4b5664bfc422e9c17d43fc296c35aa6",
+    "url": "/static/anonymousAnimals/Auroch.png"
+  },
+  {
+    "revision": "bad1b1309d3d9c0d1ff6b321e48436bd",
+    "url": "/static/anonymousAnimals/Axolotl.png"
+  },
+  {
+    "revision": "60dcbbf0807c93f900ecf6e972e83189",
+    "url": "/static/anonymousAnimals/Badger.png"
+  },
+  {
+    "revision": "bb710c20cbb0ab8da3d139fd014c750a",
+    "url": "/static/anonymousAnimals/Bat.png"
+  },
+  {
+    "revision": "d558432aec1f41e00e335dd9379e01cd",
+    "url": "/static/anonymousAnimals/Beaver.png"
+  },
+  {
+    "revision": "458a16ea9016f1fb5f9d9a175212bf95",
+    "url": "/static/anonymousAnimals/Buffalo.png"
+  },
+  {
+    "revision": "ae693c7f863d4086766647e20ab4a17c",
+    "url": "/static/anonymousAnimals/Camel.png"
+  },
+  {
+    "revision": "4f8c6954b781131758a46ce5b983a11a",
+    "url": "/static/anonymousAnimals/Capybara.png"
+  },
+  {
+    "revision": "149efb73c2240a121d8df09714638203",
+    "url": "/static/anonymousAnimals/Chameleon.png"
+  },
+  {
+    "revision": "c915ac0d1952c6461215de3ca2f4b0ba",
+    "url": "/static/anonymousAnimals/Cheetah.png"
+  },
+  {
+    "revision": "c9fbb8238948bbe88ba566017ce1a4ce",
+    "url": "/static/anonymousAnimals/Chinchilla.png"
+  },
+  {
+    "revision": "678a76b53ebb35a2b1ffa93d3a58fb5a",
+    "url": "/static/anonymousAnimals/Chipmunk.png"
+  },
+  {
+    "revision": "07c2b1cf03053e8e47ad5b60308ea8ef",
+    "url": "/static/anonymousAnimals/Chupacabra.png"
+  },
+  {
+    "revision": "e0ee4eea35eb4f7a8b44a7751f1d4771",
+    "url": "/static/anonymousAnimals/Cormorant.png"
+  },
+  {
+    "revision": "56b616d21a129e81eb9519b0be402237",
+    "url": "/static/anonymousAnimals/Coyote.png"
+  },
+  {
+    "revision": "d38f17590deb38d7a02c23851c42ce74",
+    "url": "/static/anonymousAnimals/Crow.png"
+  },
+  {
+    "revision": "3a10b59bdb1a5bda79e77c3e2c45eed2",
+    "url": "/static/anonymousAnimals/Dingo.png"
+  },
+  {
+    "revision": "bf2fc3b06c99dce51c27e7316a457955",
+    "url": "/static/anonymousAnimals/Dinosaur.png"
+  },
+  {
+    "revision": "4bf0fa216ad438098520c9a05210b67b",
+    "url": "/static/anonymousAnimals/Dolphin.png"
+  },
+  {
+    "revision": "06b7a4682536801cd0c7424b58309373",
+    "url": "/static/anonymousAnimals/Duck.png"
+  },
+  {
+    "revision": "3f7c6670c8e81e4057c5a8735eb7461d",
+    "url": "/static/anonymousAnimals/Elephant.png"
+  },
+  {
+    "revision": "c08018bbfff34e8dfdbb776685368d53",
+    "url": "/static/anonymousAnimals/Ferret.png"
+  },
+  {
+    "revision": "fa3df577363f41901da8c7b922b66a3f",
+    "url": "/static/anonymousAnimals/Fox.png"
+  },
+  {
+    "revision": "f0b1aef64ada418b64dd50808a94e3fc",
+    "url": "/static/anonymousAnimals/Frog.png"
+  },
+  {
+    "revision": "c7aa00f089ec6ba47ca66b16b6db4633",
+    "url": "/static/anonymousAnimals/Giraffe.png"
+  },
+  {
+    "revision": "1f9b7dd8bcabc28ccc9a00db8e369db4",
+    "url": "/static/anonymousAnimals/Gopher.png"
+  },
+  {
+    "revision": "7cdbdf4ecc6aef612645305e19319bc1",
+    "url": "/static/anonymousAnimals/Grizzly.png"
+  },
+  {
+    "revision": "a050869cdead80a52ded7517a459502b",
+    "url": "/static/anonymousAnimals/Hedgehog.png"
+  },
+  {
+    "revision": "70bb75f5854d383225c65c8f3c392047",
+    "url": "/static/anonymousAnimals/Hippo.png"
+  },
+  {
+    "revision": "e23be6b746cb20cd60046338540c361d",
+    "url": "/static/anonymousAnimals/Hyena.png"
+  },
+  {
+    "revision": "ebef532b7ac2999f1153a1487f4874f3",
+    "url": "/static/anonymousAnimals/Ibex.png"
+  },
+  {
+    "revision": "3a5592819b5e0b53428bc27742b23242",
+    "url": "/static/anonymousAnimals/Ifrit.png"
+  },
+  {
+    "revision": "f72613f9068bb3d9d3fae6888c188219",
+    "url": "/static/anonymousAnimals/Iguana.png"
+  },
+  {
+    "revision": "cee4835e378de2b0d35ca1d5ed1ad4a2",
+    "url": "/static/anonymousAnimals/Jackal.png"
+  },
+  {
+    "revision": "e7eb63feaaa2a66fd3dfdb1d16cc68ae",
+    "url": "/static/anonymousAnimals/Kangaroo.png"
+  },
+  {
+    "revision": "1313b5f1811f3a708a7d4a8f5644434b",
+    "url": "/static/anonymousAnimals/Koala.png"
+  },
+  {
+    "revision": "19562ab0e4a9e0714961a586c7614d59",
+    "url": "/static/anonymousAnimals/Kraken.png"
+  },
+  {
+    "revision": "e2270aa78dce03b7c4978fd8547b100d",
+    "url": "/static/anonymousAnimals/Lemur.png"
+  },
+  {
+    "revision": "9a9599103f1389c97e02a4f71f911810",
+    "url": "/static/anonymousAnimals/Leopard.png"
+  },
+  {
+    "revision": "a4acfef5254ebf576d874f0448c1b814",
+    "url": "/static/anonymousAnimals/Liger.png"
+  },
+  {
+    "revision": "b12100e6b2737b9ae6ae5cbb8a9be060",
+    "url": "/static/anonymousAnimals/Llama.png"
+  },
+  {
+    "revision": "1b907028a4a924387ff0c93f59f77593",
+    "url": "/static/anonymousAnimals/Manatee.png"
+  },
+  {
+    "revision": "107ca01054e46d5aafd351caaebf6916",
+    "url": "/static/anonymousAnimals/Mink.png"
+  },
+  {
+    "revision": "766cc572b9ee063e86173cff96c5401f",
+    "url": "/static/anonymousAnimals/Monkey.png"
+  },
+  {
+    "revision": "a3243a83e970cc0fa5a516a7a28a5048",
+    "url": "/static/anonymousAnimals/Moose.png"
+  },
+  {
+    "revision": "7a70ef99fdadffdf76608c3a590f598f",
+    "url": "/static/anonymousAnimals/Narwhal.png"
+  },
+  {
+    "revision": "36af582a692e512448054d2f219e7814",
+    "url": "/static/anonymousAnimals/Nyan Cat.png"
+  },
+  {
+    "revision": "bd7a2977dcf0d581425903f6884d7181",
+    "url": "/static/anonymousAnimals/Orangutan.png"
+  },
+  {
+    "revision": "e78b54898ecee1297ead55e40e4d5ed9",
+    "url": "/static/anonymousAnimals/Otter.png"
+  },
+  {
+    "revision": "9b061df1605115f1bd94f2d4afcacb73",
+    "url": "/static/anonymousAnimals/Panda.png"
+  },
+  {
+    "revision": "8b505347c6d4824ea38e66ea22daad5c",
+    "url": "/static/anonymousAnimals/Penguin.png"
+  },
+  {
+    "revision": "d513b1d8e5ace719dae2b224b685a13b",
+    "url": "/static/anonymousAnimals/Platypus.png"
+  },
+  {
+    "revision": "4c7361c63f570512781316753e239bda",
+    "url": "/static/anonymousAnimals/Pumpkin.png"
+  },
+  {
+    "revision": "4316cc62b535573721aa1239169792be",
+    "url": "/static/anonymousAnimals/Python.png"
+  },
+  {
+    "revision": "1d6fd53e00946223f8654a592a218f54",
+    "url": "/static/anonymousAnimals/Quagga.png"
+  },
+  {
+    "revision": "511a9836fadb6ccf334a259201a242e4",
+    "url": "/static/anonymousAnimals/Rabbit.png"
+  },
+  {
+    "revision": "31d4b2cffd4efe5033de3d8bc3b2705c",
+    "url": "/static/anonymousAnimals/Raccoon.png"
+  },
+  {
+    "revision": "064fea5cd4277fa3d888d17bddf65a02",
+    "url": "/static/anonymousAnimals/Rhino.png"
+  },
+  {
+    "revision": "e775091f326973bcfc7748789346d262",
+    "url": "/static/anonymousAnimals/Sheep.png"
+  },
+  {
+    "revision": "943c8e7cdfd05ac9c6a3c9e64fcfe0a4",
+    "url": "/static/anonymousAnimals/Shrew.png"
+  },
+  {
+    "revision": "62ae6b32df14d5400d84b1c8ee0d8222",
+    "url": "/static/anonymousAnimals/Skunk.png"
+  },
+  {
+    "revision": "3651735207b3375143bf2851399d0743",
+    "url": "/static/anonymousAnimals/Squirrel.png"
+  },
+  {
+    "revision": "084daf68ade2c93e42ab8844b2e1935c",
+    "url": "/static/anonymousAnimals/Tiger.png"
+  },
+  {
+    "revision": "824c7b2a6b0ccc7bdea0f999210502cb",
+    "url": "/static/anonymousAnimals/Turtle.png"
+  },
+  {
+    "revision": "85f0e81764843bf8f39a5d13107e791d",
+    "url": "/static/anonymousAnimals/Walrus.png"
+  },
+  {
+    "revision": "7e1d47d4fe43b7493447ede45df328d1",
+    "url": "/static/anonymousAnimals/Wolf.png"
+  },
+  {
+    "revision": "3fdcb0c5a7b2fac423c15f29cadfb69e",
+    "url": "/static/anonymousAnimals/Wolverine.png"
+  },
+  {
+    "revision": "5a844215a57d9607f1087c9d7d364880",
+    "url": "/static/anonymousAnimals/Wombat.png"
+  },
+  {
+    "revision": "0b4076a9fccce144a6469bae996ef326",
+    "url": "/static/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "733990191480edf9c4ef9169a3f98cba",
+    "url": "/static/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "e22f183e285d262c95efccdd7fc9f1d9",
+    "url": "/static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "2f8e5f54e38951bb5c880712c9e788c2",
+    "url": "/static/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "c3cfd64bc87fa3867c77e31082bc05d2",
+    "url": "/static/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c446d74a65b3651d63083570846c3b3b",
+    "url": "/static/img/bioimage-io-logo-transparent.png"
+  },
+  {
+    "revision": "917774a8febc2361a8c7481552e1e11a",
+    "url": "/static/img/bioimage-io-logo.png"
+  },
+  {
+    "revision": "e1dd5654c3c8bcca4df6f56705b14a3c",
+    "url": "/static/img/github.png"
+  },
+  {
+    "revision": "f0d21da3f74ffbbcc644dafd7b957e57",
+    "url": "/static/img/github.svg"
+  },
+  {
+    "revision": "a12d35d30ec0dcf10639bf24e3784aaf",
+    "url": "/static/img/plus-sign.png"
+  },
+  {
+    "revision": "19c9badcf53e14e3deb0954e322c93d4",
+    "url": "/static/img/zoo-background.svg"
+  },
+  {
+    "revision": "88fe8ad8d1311594c43484acbfb3ae96",
+    "url": "/static/test_manifest.bioimage.io.json"
+  }
+]);
