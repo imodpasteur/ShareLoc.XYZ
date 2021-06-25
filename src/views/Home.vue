b677149644905a38f58c216a0ad30e6ae53c334b
Wei Ouyang <oeway007@gmail.com>
2021-06-25 14:33:29 +0200

    Use the old viewer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -960,19 +960,19 @@ export default {
       }
     },
     showResourceItemInfo(mInfo) {
-      this.$router.push({ name: "Viewer", params: { resourceId: mInfo.id } });
-      // this.showInfoDialogMode = "viewer";
-      // mInfo._focus = focus;
-      // this.selectedResourceItem = mInfo;
-      // this.infoDialogTitle = this.selectedResourceItem.name;
-      // this.viewerUrl = `https://viewer.shareloc.xyz/#/?file=${mInfo.download_url}`;
-      // this.infoDialogFullscreen = true;
-      // this.$modal.show("info-dialog");
-      // if (mInfo.id) {
-      //   const query = Object.assign({}, this.$route.query);
-      //   query.id = mInfo.id;
-      //   this.$router.replace({ query: query }).catch(() => {});
-      // }
+      // this.$router.push({ name: "Viewer", params: { resourceId: mInfo.id } });
+      this.showInfoDialogMode = "viewer";
+      mInfo._focus = focus;
+      this.selectedResourceItem = mInfo;
+      this.infoDialogTitle = this.selectedResourceItem.name;
+      this.viewerUrl = `https://viewer.shareloc.xyz/#/?file=${mInfo.download_url}`;
+      this.infoDialogFullscreen = true;
+      this.$modal.show("info-dialog");
+      if (mInfo.id) {
+        const query = Object.assign({}, this.$route.query);
+        query.id = mInfo.id;
+        this.$router.replace({ query: query }).catch(() => {});
+      }
     },
     updateStatus(status) {
       if (status.loading === true) this.showMessage("Loading...");
