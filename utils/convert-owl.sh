4273c72f9f971b4a1e186930aaf58ec9ad3638e4
baiddd <bjc4991@outlook.com>
2022-04-15 14:06:38 +0200

    add actions for ontology
    Co-authored-by: Wei Ouyang <oeway@users.noreply.github.com>

---
+++ utils/convert-owl.sh
@@ -0,0 +1,2 @@
+mkdir -p output
+robot template --template src/shareloc.tsv --prefix "shareloc: https://shareloc.xyz/#/ontology/vocabulary/" --output output/shareloc.owl
---
+++ utils/setup-robot.sh
@@ -0,0 +1,4 @@
+wget https://github.com/ontodev/robot/releases/download/v1.8.3/robot.jar 
+curl https://raw.githubusercontent.com/ontodev/robot/master/bin/robot > robot
+export PATH=$PATH:$(pwd)
+robot help
