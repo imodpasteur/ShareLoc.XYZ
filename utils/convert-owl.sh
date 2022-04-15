d8cdfd3576cee8bf6f7a0f5e2660d8f8bdcfcb78
baiddd <bjc4991@outlook.com>
2022-04-15 14:14:49 +0200

    Fix script for actions

--- utils/convert-owl.sh
+++ utils/convert-owl.sh
@@ -1,2 +1,2 @@
 mkdir -p output
-robot template --template src/shareloc.tsv --prefix "shareloc: https://shareloc.xyz/#/ontology/vocabulary/" --output output/shareloc.owl
+./robot template --template src/shareloc.tsv --prefix "shareloc: https://shareloc.xyz/#/ontology/vocabulary/" --output output/shareloc.owl
--- utils/setup-robot.sh
+++ utils/setup-robot.sh
@@ -1,4 +1,5 @@
 wget https://github.com/ontodev/robot/releases/download/v1.8.3/robot.jar 
 curl https://raw.githubusercontent.com/ontodev/robot/master/bin/robot > robot
 export PATH=$PATH:$(pwd)
-robot help
+chmod +x ./robot
+./robot help
