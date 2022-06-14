2fd7b09babe96670da668a09a1f9d9b9eb37f0f4
Jiachuan Bai <56833522+baiddd@users.noreply.github.com>
2022-06-14 22:02:34 +0200

    Add description of shareloc ontology

--- utils/convert-owl.sh
+++ utils/convert-owl.sh
@@ -1,2 +1,11 @@
 mkdir -p output
-./robot template --template src/shareloc.tsv --prefix "shareloc: https://shareloc.xyz/#/ontology/vocabulary/" --output output/shareloc.owl
+./robot template --template src/shareloc.tsv \
+--prefix "shareloc: https://shareloc.xyz/#/ontology/vocabulary/" \
+--output output/shareloc_template.owl
+
+
+./robot annotate --input output/shareloc_template.owl \
+--ontology-iri "https://raw.githubusercontent.com/imodpasteur/ShareLoc.XYZ/ontology/shareloc.owl" \
+--annotation dc:description "ShareLoc defines terms to annotate data sets from single molecule localization microscopy, including but not limited to: imaging technique, biological structures or molecules of interest, cell types, experimental condition, labeling method, fixation protocol, etc." \
+--annotation dc:title "ShareLoc" \
+--output output/shareloc.owl
